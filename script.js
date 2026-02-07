2c90f6da6b410299570e8544bb205aaba7897190
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2026-02-07 14:07:23 +1100

    Add grid sizing functionality

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-let x = 16;
-let y = 16;
-let x_size = 800/x;
-let y_size = 800/y;
+let grid_size = 16;
+let cell_size = 800/grid_size;
 
-function addDiv(target, x_size, y_size) {
+function querySize() {
+    grid_size = prompt("MEOW!");
+    cell_size = 800/grid_size;
+    resetGrid(container);
+    setGrid(container);
+}
+
+function addDiv(target, cell_size) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("sketch");
     newDiv.style.backgroundColor = "red";
-    newDiv.style.minWidth = x_size + "px";
-    newDiv.style.minHeight = y_size + "px";
+    newDiv.style.minWidth = cell_size + "px";
+    newDiv.style.minHeight = cell_size + "px";
     target.appendChild(newDiv);
     return newDiv;
 }
 
 const container = document.querySelector("#container");
 
-for (let x_iter = 0; x_iter < x; x_iter ++) {
-    for (let y_iter = 0; y_iter < y; y_iter ++) {
-        let newDiv = addDiv(container, x_size, y_size);
-        newDiv.addEventListener("pointerenter", (e) => {
-            newDiv.style.backgroundColor = "blue";
-        });
+function resetGrid(grid) {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
 }
+
+function setGrid(grid) {
+    for (let x_iter = 0; x_iter < grid_size; x_iter ++) {
+        for (let y_iter = 0; y_iter < grid_size; y_iter ++) {
+            let newDiv = addDiv(grid, cell_size);
+            newDiv.addEventListener("pointerenter", (e) => {
+                newDiv.style.backgroundColor = "blue";
+            });
+        }
+    }
+}
+
+setGrid(container);
